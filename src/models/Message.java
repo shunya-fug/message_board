8da67cfae765f6b6f86f443c10e61ebf194546bf
WST-FUJIOKA <WST-FUJIOKA@DESKTOP-EDKJ36U>
2022-04-14 16:46:14 +0900

    getAllMessagesを追加

--- src/models/Message.java
+++ src/models/Message.java
@@ -8,6 +8,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 /**
@@ -15,6 +17,9 @@ import javax.persistence.Table;
  *
  */
 @Entity
+@NamedQueries({
+		@NamedQuery(name = "getAllMessages", query = "SELECT m FROM Message AS m ORDER BY m.id DESC")
+})
 @Table(name = "messages")
 
 public class Message implements Serializable {
